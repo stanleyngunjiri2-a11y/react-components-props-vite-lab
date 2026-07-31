1c01b9ab314856be009fddf5e640ca588206efde
Stanley-1923 <stanley.ngunijiri@student.moringaschool.com>
2026-07-31 10:00:17 +0300

    prop lab

---
+++ src/components/Article.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <p>{preview}</p>
+      <p>{minutes} min read</p>
+    </article>
+  );
+}
+
+export default Article;
---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Article from "./Article.jsx";
+
+function ArticleList({ posts }) {
+  const articleComponents = posts.map((post) => (
+    <Article
+      key={post.id}
+      title={post.title}
+      date={post.date}
+      preview={post.preview}
+      minutes={post.minutes}
+    />
+  ));
+
+  
+  return <main>{articleComponents}</main>;
+}
+   
+
+export default ArticleList;
